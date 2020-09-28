bcae176d7543195b2405b0ee5acdd5797078e058
ida4594 <kaha4594@yahoo.co.jp>
2020-09-28 22:32:41 +0900

    8

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweeet.create(tweet_params)
+    Tweeet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
