017c4509595933ad5796b99bf1ad1d589a47cff8
ida4594 <kaha4594@yahoo.co.jp>
2020-10-05 23:54:37 +0900

    12

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweeet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweeet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
